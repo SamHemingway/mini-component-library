c097a07fcb04d8aa7737dba12ec4ae802b062707
Sam Hemingway <sam@Sams-MBP.ht.home>
2023-03-28 12:41:02 -0400

    ProgressBar WIP

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,19 +5,60 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size, label = "progress", ...delegated }) => {
-  const id = React.useId();
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "0",
+    "--border-radius": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "0",
+    "--border-radius": "4px",
+  },
+  large: {
+    "--height": "16px",
+    "--padding": "4px",
+    "--border-radius": "8px",
+  },
+};
 
+const ProgressBar = ({ value, size, label = "progress", ...delegated }) => {
+  const sizeStyles = SIZES[size];
+  const id = React.useId();
   return (
     <>
       <label htmlFor={id}>
         <VisuallyHidden>{label}</VisuallyHidden>
       </label>
-      <Progress value={value} max="100" {...delegated} id={id} />
+      <Wrapper style={sizeStyles}>
+        <Progress value={value} max="100" {...delegated} id={id} />
+      </Wrapper>
     </>
   );
 };
 
-const Progress = styled.progress``;
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  padding: var(--padding);
+  inline-size: min-content;
+  border-radius: var(--border-radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Progress = styled.progress`
+  background: none;
+  border: none;
+  height: var(--height);
+  border-radius: 4px;
+
+  &::-moz-progress-bar {
+    background-color: ${COLORS.primary};
+  }
+
+  &::-webkit-progress-bar {
+    background-color: ${COLORS.primary};
+  }
+`;
 
 export default ProgressBar;
