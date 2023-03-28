6254faf46b3ff46e002e5fc974ba4169b3582450
Sam Hemingway <sam@Sams-MBP.ht.home>
2023-03-28 11:51:27 -0400

    Getting help with issue

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const ProgressBar = ({ value, size, label = "progress", ...delegated }) => {
+  const id = React.useId();
+
+  return (
+    <>
+      <label htmlFor={id}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+      </label>
+      <Progress value={value} max="100" {...delegated} id={id} />
+    </>
+  );
 };
+
+const Progress = styled.progress``;
 
 export default ProgressBar;
