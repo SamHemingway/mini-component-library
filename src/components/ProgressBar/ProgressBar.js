53b155348d67134b15fb1a33a8bb48bba289ef1b
Sam Hemingway <samphemingway@icloud.com>
2023-03-28 17:09:42 -0400

    Finished Select, updated ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -25,40 +25,68 @@ const SIZES = {
 
 const ProgressBar = ({ value, size, label = "progress", ...delegated }) => {
   const sizeStyles = SIZES[size];
-  const id = React.useId();
+  // const id = React.useId();
   return (
-    <>
-      <label htmlFor={id}>
-        <VisuallyHidden>{label}</VisuallyHidden>
-      </label>
-      <Wrapper style={sizeStyles}>
-        <Progress value={value} max="100" {...delegated} id={id} />
-      </Wrapper>
-    </>
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={sizeStyles}
+    >
+      <VisuallyHidden>
+        {label}: {value}%
+      </VisuallyHidden>
+      <BarWrapper>
+        <Bar style={{ "--width": value + "%" }} />
+      </BarWrapper>
+    </Wrapper>
+
+    // <>
+    //   <label htmlFor={id}>
+    //     <VisuallyHidden>{label}</VisuallyHidden>
+    //   </label>
+    //   <Wrapper style={sizeStyles}>
+    //     <Progress value={value} max="100" {...delegated} id={id} />
+    //   </Wrapper>
+    // </>
   );
 };
 
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background: ${COLORS.primary};
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
 const Wrapper = styled.div`
   background: ${COLORS.transparentGray15};
+  inline-size: 400px;
   padding: var(--padding);
-  inline-size: min-content;
   border-radius: var(--border-radius);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  line-height: 0;
 `;
 
-const Progress = styled.progress`
-  background: none;
-  border: none;
-  height: var(--height);
-  border-radius: 4px;
+// const Progress = styled.progress`
+//   background: none;
+//   border: none;
+//   height: var(--height);
+//   border-radius: 4px;
+//   display: block;
 
-  &::-moz-progress-bar {
-    background-color: ${COLORS.primary};
-  }
+//   &::-moz-progress-bar {
+//     background-color: ${COLORS.primary};
+//   }
 
-  &::-webkit-progress-bar {
-    background-color: ${COLORS.primary};
-  }
-`;
+//   &::-webkit-progress-bar {
+//     background-color: ${COLORS.primary};
+//   }
+// `;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <Presentation>
+        {displayedValue}
+        <Icon id="chevron-down" size={24} strokeWidth={2} />
+      </Presentation>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  inline-size: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  inline-size: 100%;
+  block-size: 100%;
+  opacity: 0;
+`;
+
+const Presentation = styled.div`
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1em;
+  padding: 10px 10px 10px 16px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${NativeSelect}:focus + & {
+    outline: 2px auto -webkit-focus-ring-color;
+    outline: 2px auto highlight;
+  }
+`;
 
 export default Select;
