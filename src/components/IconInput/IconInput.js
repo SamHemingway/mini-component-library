d9360691c8b51d5e3fffb289ce199bf4f2101f0a
Sam Hemingway <samphemingway@icloud.com>
2023-03-28 21:59:09 -0400

    Update IconInput.js

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,89 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: {
+    "--font-size": "0.875rem",
+    "--padding": "2px",
+    "--gap": "12px",
+    "--stroke-weight": "1px",
+  },
+  large: {
+    "--font-size": "1.125rem",
+    "--padding": "4px",
+    "--gap": "16px",
+    "--stroke-weight": "2px",
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const id = React.useId();
+  const sizeStyles = SIZES[size];
+
+  return (
+    <Wrapper style={{ ...sizeStyles, "--width": `${width}px` }}>
+      <Label htmlFor={id}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <Icon
+          id={icon}
+          strokeWidth={size === "large" && 2}
+          size={size === "large" ? "22px" : "16px"}
+        />
+      </Label>
+      <TextInput id={id} type="text" placeholder={placeholder} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.form`
+  display: flex;
+  align-items: center;
+  gap: var(--gap);
+  border-bottom: var(--stroke-weight) solid ${COLORS.black};
+  inline-size: fit-content;
+  padding-block-start: 4px;
+  padding-block-end: var(--padding);
+  color: ${COLORS.gray700};
+  width: var(--width);
+
+  &:focus-within {
+    outline: 2px auto -webkit-focus-ring-color;
+    outline: 2px auto highlight;
+    outline-offset: 2px;
+    border-radius: 2px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Label = styled.label``;
+
+const TextInput = styled.input`
+  border: none;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  width: 100%;
+  font-size: var(--font-size);
+  ${Wrapper}:hover & {
+    color: ${COLORS.black};
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+    opacity: 1;
+  }
+`;
 
 export default IconInput;
